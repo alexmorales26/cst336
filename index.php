e44a220f912a3ea3b511532548fc68d7cb11d4c7
alexmorales26 <alexmorales@csumb.edu>
2017-08-25 20:20:48 +0000

    ading index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+    <h1> Alexander Morales CST336 </h1>
+    </body>
+</html>
